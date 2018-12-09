b35879e53a52bed8537ac65e79b19e515399d328
andriyana <andriyana2212@gmail.com>
2018-12-09 01:46:58 -0500

    added readme and logging

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # LIRI-Bot
+
+The purpose of this project was to create a LIRI bot using Node JS. LIRI is a command line app that takes in parameters and returns data based on one of four commands:
+
+- spotify-this-song
+- movie-this
+- concert-this
+- do-what-it-says
+
+What each command does:
+- spotify-this-song - displays the following information about the requested song:
+     - The song's name
+     - Artist(s)
+     - The album that the song is from 
+     - A preview link of the song from Spotify
+If the input was not provided, the app will default to "The Sign" by Ace of Base.
+
+- movie-this - displays the following information about the requested movie:
+    - Title of the movie
+    - Year the movie came out
+    - IMDB Rating of the movie
+    - Rotten Tomatoes Rating of the movie
+    - Country where the movie was produced
+    - Language of the movie
+    - Plot of the movie
+    - Actors in the movie
+If the input was not provided, the app will default to "Mr. Nobody".
+
+- concert-this - displays the following information about the concert of the requested artist:
+    - Name of the venue
+    - Venue location
+    - Date of the Event 
+
+- do-what-it-says - takes the input of the random.txt file. 
+Depending on the text in the file, the command can run either concert-this, spotify-this-song, or movie-this functions. 
+
+
+
+
+
--- liri.js
+++ liri.js
@@ -12,27 +12,41 @@ var fs = require("fs");
 var search = process.argv[2];
 var term = process.argv.slice(3).join(" ");
 
+
+var divider = "\n----------------------"
+
+//---------------
 // Searches 
+//---------------
 if (search === "movie-this") {
-    console.log("Movie: \n____________________________");
+    console.log("Looking for Your Movie..." + divider);
     movieThis();
 
 } else if (search === "spotify-this-song") {
-    console.log("Song: \n____________________________");
+    console.log("Looking for Your Song..." + divider);
     spotifyThis();
 
 } else if (search === "concert-this") {
-    console.log("Concert: \n____________________________");
+    console.log("Looking for Your Concert..." + divider);
     concertThis();
 
 } else if(search === "do-what-it-says") {
-    console.log("Randomness: \n____________________________");
+    console.log("Looking for Random Things..." + divider);
     doWhat();
 }
 
+//---------------
 // Search movie-this
+//---------------
 function movieThis() {
     var movie = term
+
+    // if no movie was entered, the default movie that will show up will be 
+    // "Mr. Nobody" 
+    if (movie === "") {
+        movie = "Mr. Nobody";
+    }
+
     var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
     axios
@@ -63,12 +77,26 @@ function movieThis() {
 
             var movieCast = response.data.Actors;
             console.log("Cast: " + movieCast)
+
+            console.log(divider)
         });
+
+         fs.appendFile("log.txt", "\n" + search + "\n" + movie + divider, function(err) {
+            if (err) throw err;
+         });
 };
 
+//---------------
 // Search spotify-this-song
+//---------------
 function spotifyThis() {
     var song = term;
+
+    // if no song was entered, the default song that will show up will be 
+    // "Sorry Not Sorry" by Demi Lovato
+    if (song === "") {
+        song = "The Sign";
+    }
 
     spotify
         .search({ type: 'track', query: song })
@@ -89,13 +117,21 @@ function spotifyThis() {
 
             var songLink = songInfo.external_urls.spotify
             console.log("Link: " + songLink)
+
+            console.log(divider)
         })
         .catch(function(err) {
             console.log(err);
         });
+
+        fs.appendFile("log.txt", "\n" +  search + "\n" + song + divider, function(err) {
+            if (err) throw err;
+         });
 };
 
+//---------------
 // Search concert-this
+//---------------
 function concertThis() {
     var artist = term
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
@@ -107,16 +143,25 @@ function concertThis() {
             var concertInfo = response.data
             // console.log(concertInfo)
 
-            var venue = concertInfo[0].venue.name;
-            console.log("Venue: " + venue)
+             for (i = 0; i < concertInfo.length; i++) {
 
-            var venueLoc = concertInfo[0].venue.city;
-            console.log("Venue Location: " + venueLoc)
+            var concertVenue = concertInfo[i].venue.name;
+            console.log("Venue: " + concertVenue)
 
-            var venueDate = concertInfo[0].datetime;
-            venueDate = moment(venueDate).format("dddd, MMMM Do YYYY, h:mm A")
-            console.log("Venue Date: " + venueDate)
+            var concertLocation = concertInfo[i].venue.city;
+            console.log("Location: " + concertLocation)
+
+            var concertDate = concertInfo[i].datetime;
+            concertDate = moment(concertDate).format("MM/DD/YYYY")
+            console.log("Concert Date: " + concertDate)
+
+            console.log(divider)
+             }
         });
+
+        fs.appendFile("log.txt", "\n" + search + "\n" + artist + divider, function(err) {
+            if (err) throw err;
+         });
 };
 
 // Search do-what-it-says
@@ -129,23 +174,19 @@ function doWhat() {
       // console.log(grabName)
 
       if (randomFile[0] === "spotify-this-song") {
-        spotifyThis(term = grabName);
+        spotifyThis(song = grabName);
 
       } else if (randomFile[0] === "movie-this") {
-        movieThis(term = grabName);
+        movieThis(movie = grabName);
 
       } else if (randomFile[0] === "concert-this") {
-        concertThis(term = grabName);
+        concertThis(artist = grabName);
       }; 
 
       if (error) {
-        console.log(error);
+        return console.log(error);
       }; 
        
     });
 };
 
-// If no searches were entered, the default information shown on the page should be as follows
-// if (!term) {
-// term = "Mr. Nobody";
-// }
